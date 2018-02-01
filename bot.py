a65eb3bfe4f203ac4e0d41db07289a627aaa163b
Gabriel Francisco <gabfssilva@gmail.com>
2018-02-01 19:39:05 -0200

    moving all corpus yml files to /corpus

--- bot.py
+++ bot.py
@@ -19,15 +19,7 @@ class ChatBot:
 		)
 
 		self.chatbot.train("chatterbot.corpus.portuguese")
-		self.chatbot.train(
-		    "./questions.yml",
-		    "./cupom.yml",
-		    "./lost_items.yml",
-			"./rate_driver.yml",
-			"./wrong_estimative.yml",
-			"./ride_finished_after.yml",
-			"./wrong_path.yml"
-		)
+		self.chatbot.train("./corpus")
 
 	def answer(self, conversation_id, question):
 	    response = self.chatbot.get_response(question)
