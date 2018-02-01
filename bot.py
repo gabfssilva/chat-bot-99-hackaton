d920047a7c24398a23c1a98e4d2306efeecf2a4a
Gabriel Ozeas <gabriel.ozeas1@gmail.com>
2018-02-01 18:01:15 -0200

    Adding endpoint to bot

--- bot.py
+++ bot.py
@@ -1,34 +1,28 @@
-from chatterbot import ChatBot
+import chatterbot 
 
-chatbot = ChatBot(
-    'Ron Obvious',
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
-)
+class ChatBot:
+	def __init__(self):
+		self.chatbot = chatterbot.ChatBot(
+		    'Ron Obvious',
+		    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+		)
 
-chatbot.train("chatterbot.corpus.portuguese")
+		self.chatbot.train("chatterbot.corpus.portuguese")
+		self.chatbot.train(
+		    "./questions.yml",
+		    "./cupom.yml",
+		    "./lost_items.yml"
+		)
 
-chatbot.train(
-    "/Users/gabriel.francisco/workspace/hackaton/chatterbot-sample/questions.yml",
-    "/Users/gabriel.francisco/workspace/hackaton/chatterbot-sample/cupom.yml",
-    "/Users/gabriel.francisco/workspace/hackaton/chatterbot-sample/lost_items.yml"
-)
+	def answer(self, conversation_id, question):
+	    response = self.chatbot.get_response(question)
+	    return response
 
-def get_response(question):
-    print("User: " + question)
-    print(chatbot.get_response(question))
-
-get_response("Oi, como você tá?")
-get_response("Bem também.")
-get_response("Estou com um problema, pode me ajudar?")
-get_response("então, eu acabei de receber uma cobrança indevida")
-get_response("acho que foi durante o cadastro")
-get_response("Ah, obrigado então!")
-get_response("Vish, acho que tenho mais uma dúvida.")
-get_response("A tem mais uma coisa, esqueci meu guarda-chuva no carro")
-
-
-
-
-# print(chatbot.get_response("Como posso adicionar novos cupons?"))
-# print(chatbot.get_response("E pra ver os meus cupons?"))
-# print(chatbot.get_response("É só isso. Obrigado!"))
+# get_response("Oi, como você tá?")
+# get_response("Bem também.")
+# get_response("Estou com um problema, pode me ajudar?")
+# get_response("então, eu acabei de receber uma cobrança indevida")
+# get_response("acho que foi durante o cadastro")
+# get_response("Ah, obrigado então!")
+# get_response("Vish, acho que tenho mais uma dúvida.")
+# get_response("A tem mais uma coisa, esqueci meu guarda-chuva no carro")
---
+++ endpoints.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+from bot import ChatBot
+
+import uuid
+
+app = Flask(__name__)
+
+chatbot = ChatBot()
+
+@app.route('/bot', methods=['POST'])
+def answer():
+    body = request.get_json(force=True)
+
+    conversation_id = body.get("conversation_id", str(uuid.uuid1()))
+    question = body['question']
+
+    answer = chatbot.answer(conversation_id, question)
+
+    response = {}
+    response['question'] = question
+    response['conversation_id'] = conversation_id
+    response['answer'] = answer.text
+
+    return jsonify(response)
+
+app.run()
