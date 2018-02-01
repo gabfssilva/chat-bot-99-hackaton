5d04f15caef5f449eabd13d2e90c4e42a50747b4
Gabriel Francisco <gabfssilva@gmail.com>
2018-02-01 21:48:39 -0200

    LowConfidenceAdapter added

--- bot.py
+++ bot.py
@@ -15,7 +15,17 @@ class ChatBot:
 
 		self.chatbot = chatterbot.ChatBot(
 		    'Ron Obvious',
-		    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+		    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+			logic_adapters=[
+              {
+                'import_path': 'chatterbot.logic.BestMatch'
+              },
+              {
+                'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+                'threshold': 0.70,
+                'default_response': 'Eu não entendi. Bem, eu sou um robozinho meio lerdo e preciso que você explique de uma forma detalhada com palavras chaves para que eu entenda melhor. Pode refazer a sua pergunta?'
+              }
+            ]
 		)
 
 		self.chatbot.train("chatterbot.corpus.portuguese")
